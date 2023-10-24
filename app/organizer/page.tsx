74ea58b9b4461934b650af3201998b402e94ea21
Charles Ji <me@charlesji.com>
2023-10-24 11:53:58 -0400

    Show real events in organizer dashboard

--- app/organizer/page.tsx
+++ app/organizer/page.tsx
@@ -6,8 +6,12 @@ import { Button } from '@/components/ui/button'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Textarea } from '@/components/ui/textarea'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LineChart, Line } from 'recharts'
+import { useCreateEvent, useTransactionReceipt, useGetAllEvents, useEventTicketData } from '@/lib/hooks'
+import { useAccount } from 'wagmi'
+import { parseEther, formatEther } from 'viem'
+import { useState } from 'react'
+import { Badge } from '@/components/ui/badge'
 
 const salesData = [
   { date: 'Jan', primary: 40, secondary: 10 },
@@ -23,7 +27,85 @@ const revenueData = [
   { month: 'Apr', revenue: 9.5 },
 ]
 
+function EventManagementCard({ eventAddress }: { eventAddress: `0x${string}` }) {
+  const { address } = useAccount()
+  const { eventName, eventDate, basePrice, totalSupply, organizer } = useEventTicketData(eventAddress)
+
+  // Only show if current user is the organizer
+  if (organizer !== address) return null
+
+  const date = eventDate ? new Date(Number(eventDate) * 1000) : null
+  const sold = totalSupply ? Number(totalSupply) : 0
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{eventName || 'Event'}</CardTitle>
+        <CardDescription className="font-mono text-xs truncate">{eventAddress}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid grid-cols-4 gap-4 text-center">
+          <div>
+            <div className="text-2xl font-bold">{sold}</div>
+            <div className="text-sm text-muted-foreground">Sold</div>
+          </div>
+          <div>
+            <div className="text-2xl font-bold">{date ? date.toLocaleDateString() : 'TBD'}</div>
+            <div className="text-sm text-muted-foreground">Date</div>
+          </div>
+          <div>
+            <div className="text-2xl font-bold">{basePrice ? formatEther(basePrice) : '0'} ETH</div>
+            <div className="text-sm text-muted-foreground">Base Price</div>
+          </div>
+          <div>
+            <div className="text-2xl font-bold">{basePrice && sold ? formatEther(basePrice * BigInt(sold)) : '0'} ETH</div>
+            <div className="text-sm text-muted-foreground">Revenue</div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
 export default function OrganizerPage() {
+  const { address } = useAccount()
+  const { createEvent, hash, isPending } = useCreateEvent()
+  const { isSuccess } = useTransactionReceipt(hash)
+  const { data: events } = useGetAllEvents()
+
+  const [formData, setFormData] = useState({
+    name: '',
+    symbol: '',
+    eventName: '',
+    date: '',
+    basePrice: '',
+    maxPriceMultiplier: '150',
+    minHoldPeriod: '86400',
+    maxTransfers: '2',
+    royaltyBps: '500',
+  })
+
+  const handleSubmit = () => {
+    if (!address) return
+
+    const eventDate = Math.floor(new Date(formData.date).getTime() / 1000)
+
+    createEvent({
+      name: formData.name,
+      symbol: formData.symbol,
+      eventName: formData.eventName,
+      eventDate: BigInt(eventDate),
+      basePrice: parseEther(formData.basePrice),
+      organizer: address,
+      resaleConfig: {
+        maxPriceMultiplier: BigInt(formData.maxPriceMultiplier),
+        minHoldPeriod: BigInt(formData.minHoldPeriod),
+        maxTransfers: BigInt(formData.maxTransfers),
+        royaltyBps: BigInt(formData.royaltyBps),
+      },
+    })
+  }
+
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
@@ -47,73 +129,137 @@ export default function OrganizerPage() {
               <CardHeader>
                 <CardTitle>Create New Event</CardTitle>
                 <CardDescription>
-                  Deploy a new ticketing contract with custom anti-scalping rules
+                  Deploy a new NFT ticketing contract with anti-scalping rules
                 </CardDescription>
               </CardHeader>
               <CardContent className="space-y-4">
                 <div className="grid grid-cols-2 gap-4">
-                  <div className="space-y-2">
+                  <div>
+                    <Label>Token Name</Label>
+                    <Input
+                      placeholder="Summer Festival Ticket"
+                      value={formData.name}
+                      onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                    />
+                  </div>
+                  <div>
+                    <Label>Token Symbol</Label>
+                    <Input
+                      placeholder="SFT"
+                      value={formData.symbol}
+                      onChange={(e) => setFormData({ ...formData, symbol: e.target.value })}
+                    />
+                  </div>
+                  <div className="col-span-2">
                     <Label>Event Name</Label>
-                    <Input placeholder="Summer Music Festival" />
+                    <Input
+                      placeholder="Summer Music Festival 2025"
+                      value={formData.eventName}
+                      onChange={(e) => setFormData({ ...formData, eventName: e.target.value })}
+                    />
                   </div>
-                  <div className="space-y-2">
-                    <Label>Venue</Label>
-                    <Input placeholder="Central Park, NYC" />
-                  </div>
-                  <div className="space-y-2">
+                  <div>
                     <Label>Date</Label>
-                    <Input type="date" />
+                    <Input
+                      type="date"
+                      value={formData.date}
+                      onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+                    />
                   </div>
-                  <div className="space-y-2">
+                  <div>
                     <Label>Base Price (ETH)</Label>
-                    <Input type="number" step="0.01" placeholder="0.1" />
-                  </div>
-                  <div className="space-y-2">
-                    <Label>Total Supply</Label>
-                    <Input type="number" placeholder="100" />
-                  </div>
-                  <div className="space-y-2">
-                    <Label>Max Resale Multiplier</Label>
-                    <Input type="number" placeholder="150" />
+                    <Input
+                      type="number"
+                      step="0.01"
+                      placeholder="0.1"
+                      value={formData.basePrice}
+                      onChange={(e) => setFormData({ ...formData, basePrice: e.target.value })}
+                    />
                   </div>
                 </div>
+
                 <div className="space-y-2">
-                  <Label>Description</Label>
-                  <Textarea placeholder="Event description..." />
+                  <h3 className="font-semibold">Anti-Scalping Rules</h3>
+                  <div className="grid grid-cols-2 gap-4">
+                    <div>
+                      <Label>Max Resale Multiplier (%)</Label>
+                      <Input
+                        type="number"
+                        value={formData.maxPriceMultiplier}
+                        onChange={(e) => setFormData({ ...formData, maxPriceMultiplier: e.target.value })}
+                      />
+                      <p className="text-xs text-muted-foreground">150 = 1.5x max resale</p>
+                    </div>
+                    <div>
+                      <Label>Hold Period (seconds)</Label>
+                      <Input
+                        type="number"
+                        value={formData.minHoldPeriod}
+                        onChange={(e) => setFormData({ ...formData, minHoldPeriod: e.target.value })}
+                      />
+                      <p className="text-xs text-muted-foreground">86400 = 1 day</p>
+                    </div>
+                    <div>
+                      <Label>Max Transfers</Label>
+                      <Input
+                        type="number"
+                        value={formData.maxTransfers}
+                        onChange={(e) => setFormData({ ...formData, maxTransfers: e.target.value })}
+                      />
+                    </div>
+                    <div>
+                      <Label>Royalty (basis points)</Label>
+                      <Input
+                        type="number"
+                        value={formData.royaltyBps}
+                        onChange={(e) => setFormData({ ...formData, royaltyBps: e.target.value })}
+                      />
+                      <p className="text-xs text-muted-foreground">500 = 5%</p>
+                    </div>
+                  </div>
                 </div>
-                <Button className="w-full">Deploy Event Contract</Button>
+
+                <Button
+                  className="w-full"
+                  size="lg"
+                  onClick={handleSubmit}
+                  disabled={!address || isPending || !formData.name || !formData.basePrice || !formData.date}
+                >
+                  {isPending ? 'Creating Event...' : isSuccess ? 'Success! ✓' : 'Deploy Event Contract'}
+                </Button>
+
+                {isSuccess && (
+                  <div className="p-4 bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-800 rounded-lg">
+                    <p className="text-sm text-green-600 dark:text-green-400 font-medium">
+                      Event created successfully! Check "My Events" tab to see it.
+                    </p>
+                  </div>
+                )}
+
+                {!address && (
+                  <p className="text-sm text-center text-muted-foreground">
+                    Connect your wallet to create events
+                  </p>
+                )}
               </CardContent>
             </Card>
           </TabsContent>
 
           <TabsContent value="manage">
             <div className="grid gap-4">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Summer Music Festival 2025</CardTitle>
-                  <CardDescription>Jul 15, 2025 • Central Park, NYC</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <div className="grid grid-cols-4 gap-4 text-center">
-                    <div>
-                      <div className="text-2xl font-bold">45</div>
-                      <div className="text-sm text-muted-foreground">Sold</div>
-                    </div>
-                    <div>
-                      <div className="text-2xl font-bold">55</div>
-                      <div className="text-sm text-muted-foreground">Available</div>
-                    </div>
-                    <div>
-                      <div className="text-2xl font-bold">4.5 ETH</div>
-                      <div className="text-sm text-muted-foreground">Revenue</div>
-                    </div>
-                    <div>
-                      <div className="text-2xl font-bold">0.2 ETH</div>
-                      <div className="text-sm text-muted-foreground">Royalties</div>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
+              {!address ? (
+                <div className="text-center py-12">
+                  <p className="text-muted-foreground">Connect your wallet to view your events</p>
+                </div>
+              ) : events && events.length > 0 ? (
+                (events as `0x${string}`[]).map((eventAddress) => (
+                  <EventManagementCard key={eventAddress} eventAddress={eventAddress} />
+                ))
+              ) : (
+                <div className="text-center py-12">
+                  <p className="text-muted-foreground">No events found</p>
+                </div>
+              )}
             </div>
           </TabsContent>
 
