fda3d16a39fe7f3bdfefc284ff9e6af950001621
Charles Ji <me@charlesji.com>
2025-11-04 11:23:21 -0500

    add inject

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,15 +4,23 @@ import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { config } from '@/lib/wagmi'
 import { useState } from 'react'
+import { InjectProvider } from "@refract-network/inject";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
-    </WagmiProvider>
+    <InjectProvider
+      appId={"cookie-shark"}
+      parentOrigin={
+        process.env.NEXT_PUBLIC_PARENT_APP_ORIGIN || "http://localhost:3000"
+      }
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </WagmiProvider>
+    </InjectProvider>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: [
+    "@refract-network/inject",
+    "@refract-network/secure-sign",
+    "@refract-network/core",
+  ],
 };
 
 export default nextConfig;
